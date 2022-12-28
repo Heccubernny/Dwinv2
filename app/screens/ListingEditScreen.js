8dbb273d71944376dce66001aa79f719bc03ff84
Ikubanni Paul <ipom4eva@gmail.com>
2022-12-28 21:28:39 +0100

    updating. error at findhouse flatlist is not render successfully

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -58,9 +58,9 @@ export default function ListingEditScreen({ navigation })
             const granted = await PermissionsAndroid.request(
                 PermissionsAndroid.PERMISSIONS.CAMERA,
                 {
-                    title: "Cool Photo App Camera Permission",
+                    title: " Camera Permission",
                     message:
-                        "Cool Photo App needs access to your camera " +
+                        "App needs access to your camera " +
                         "so you can take awesome pictures.",
                     buttonNeutral: "Ask Me Later",
                     buttonNegative: "Cancel",
